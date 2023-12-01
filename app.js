053288491830a9b996d2ec203ca374590d4191ab
hlee1502 <lee.hannah2@northeastern.edu>
2023-11-30 19:24:02 -0500

    a6 mongo

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
 import session from "express-session";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 
+mongoose.connect("mongodb://localhost:27017/kanbas");
 const app = express();
-app.use(cors({credentials: true, origin: "*"}));
+app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
+const sessionOptions = {secret: "any string", resave: false, saveUninitialized: false,};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {sameSite: "none", secure: true,};
+}
+app.use(session(sessionOptions));
 app.use(express.json());
+UserRoutes(app);
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
